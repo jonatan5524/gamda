7954700da8a3f8bfed5331eca4072bd5efdf890b
jonatan <jonatan5524@gmail.com>
2022-04-19 00:01:51 +0300

    test(slice): adding unit test to any

--- internal/slice/any_test.go
+++ internal/slice/any_test.go
@@ -19,6 +19,39 @@ func TestAny_MatchFirstElement(t *testing.T) {
 	}
 }
 
+func TestAny_MatchFirstElementStruct(t *testing.T) {
+	type twoNums struct {
+		numA int
+		numB int
+	}
+
+	arr := []twoNums{
+		{
+			numA: 1,
+			numB: 2,
+		},
+		{
+			numA: 3,
+			numB: 8,
+		},
+		{
+			numA: 5,
+			numB: 6,
+		},
+	}
+	expected := true
+
+	isAThree := func(twoNums twoNums) bool {
+		return twoNums.numA == 3
+	}
+
+	actual := Any(isAThree, arr)
+
+	if expected != actual {
+		t.Errorf("TestFind_MatchFirstElement failed. Expected slice: %v, actual: %v", expected, actual)
+	}
+}
+
 func TestAny_NoElementMatch(t *testing.T) {
 	arr := []int{1, 3, 5, 7}
 	expected := false
