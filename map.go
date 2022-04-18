beea6b9386d2424e4efb00f56e221987167e9318
jonatan <jonatan5524@gmail.com>
2022-04-18 15:09:59 +0300

    feat(map)

---
+++ map.go
@@ -0,0 +1,16 @@
+package gamda
+
+type mapByFunc[T any, S any] func(elem T) S
+
+// Map function receives a mapby function and a slice and returns mapped slice by mapby.
+// The mapby function receives an element and returns new element by mapby
+// The slice type needs to be the same type of the mapby parameter
+func Map[T any, S any](mapBy mapByFunc[T, S], arr []T) []S {
+	mapArr := make([]S, 0)
+
+	for _, elem := range arr {
+		mapArr = append(mapArr, mapBy(elem))
+	}
+
+	return mapArr
+}
---
+++ map_test.go
@@ -0,0 +1,74 @@
+package gamda_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jonatan5524/gamda"
+)
+
+type numBelowZero struct {
+	number    int
+	belowZero bool
+}
+
+func doubleElems(elem int) int {
+	return elem * 2
+}
+
+func convertToNumBelowZero(elem int) numBelowZero {
+	return numBelowZero{
+		number:    elem,
+		belowZero: elem < 0,
+	}
+}
+
+func TestMap_ElementsDoubles(t *testing.T) {
+	arr := []int{1, 2, 3, 4}
+	expected := []int{2, 4, 6, 8}
+
+	actual := gamda.Map(doubleElems, arr)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("TestMap_ElementsDoubles failed. Expected slice: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestMap_ConvertElementType(t *testing.T) {
+	arr := []int{-1, 2, -3, 4}
+	expected := []numBelowZero{
+		{
+			number:    -1,
+			belowZero: true,
+		},
+		{
+			number:    2,
+			belowZero: false,
+		},
+		{
+			number:    -3,
+			belowZero: true,
+		},
+		{
+			number:    4,
+			belowZero: false,
+		},
+	}
+
+	actual := gamda.Map(convertToNumBelowZero, arr)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("TestMap_ElementsDoubles failed. Expected slice: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestMap_EmptySlice(t *testing.T) {
+	arr := []int{}
+	expected := []int{}
+
+	actual := gamda.Map(doubleElems, arr)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("TestMap_EmptySlice failed. Expected slice: %v, actual: %v", expected, actual)
+	}
+}
